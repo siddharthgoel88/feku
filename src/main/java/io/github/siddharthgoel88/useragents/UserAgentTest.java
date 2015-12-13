49643cf5a7bff06c8671aee2f769214142bb368d
Siddharth Goel <siddharth98391@gmail.com>
2015-12-14 00:17:27 +0800

    Added a simple test file to test generated code
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

---
+++ src/main/java/io/github/siddharthgoel88/useragents/UserAgentTest.java
@@ -0,0 +1,11 @@
+package io.github.siddharthgoel88.useragents;
+
+import io.github.siddharthgoel88.useragents.impl.Feku;
+
+public class UserAgentTest {
+
+	public static void main(String[] args) {
+		System.out.println(Feku.getChrome().getRandomUserAgent());
+	}
+	
+}

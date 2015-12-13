b8b84bd30a86b0c90222399931fbc3e27ab00de5
Siddharth Goel <siddharth98391@gmail.com>
2015-12-14 00:07:49 +0800

    Added Base class for all user agents
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

---
+++ src/main/java/io/github/siddharthgoel88/useragents/UserAgent.java
@@ -0,0 +1,64 @@
+package io.github.siddharthgoel88.useragents;
+
+import java.util.Random;
+
+/**
+ * An abstract class which needs to be extended by 
+ * all the software components for which Feku will 
+ * be returning the user agent. The java code for 
+ * all these software components is auto-generated.
+ *
+ */
+public abstract class UserAgent {
+	
+	public UserAgent() {
+		userAgentStrings = getUserAgentStrings();
+		numberOfUserAgentStrings = userAgentStrings.length;
+		generator = new Random();
+	}
+	
+	private String userAgentStrings[];
+	private int numberOfUserAgentStrings;
+	private Random generator; 
+	
+	/**
+	 * Returns the complete list of user agent strings
+	 * of a software component.
+	 * 
+	 * @return list of user agent strings
+	 */
+	protected abstract String[] getUserAgentStrings();
+	
+	/**
+	 * Returns the user agent which was latest when 
+	 * this version of Feku was released.
+	 * 
+	 * @return latest user agent
+	 */
+	public String getLatestUserAgent() {
+		return userAgentStrings[0];
+	}
+	
+	/**
+	 * Returns any random user agent among the list of
+	 * user agents.
+	 * 
+	 * @return a random user agent
+	 */
+	public String getRandomUserAgent() {
+		int randomNumber = generator.
+				nextInt(numberOfUserAgentStrings);
+		return userAgentStrings[randomNumber];
+	}
+	
+	/**
+	 * A convenience API for getting user agent string. 
+	 * Currently it returns the latest user agent.
+	 * 
+	 * @return
+	 */
+	public String getUserAgent() {
+		return getLatestUserAgent();
+	}
+	
+}
